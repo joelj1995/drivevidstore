69e548757f7d12feee9327c59935899bf569b123
Joel Johnston <colter13@gmail.com>
2021-12-14 18:44:23 -0700

    Implement firebase login

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 
 import { environment } from '../environments/environment';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 import { LoginComponent } from './login/login.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
@@ -23,8 +27,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    FormsModule,
     MatToolbarModule,
     MatIconModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
     AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
--- app/src/app/login/login.component.ts
+++ app/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
+
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string = '';
+  password: string = '';
+
+  constructor(private fireAuth: AngularFireAuth, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  submitLogin(e) {
+    e.preventDefault();
+    this.fireAuth.signInWithEmailAndPassword(this.email, this.password)
+      .then(value => {
+        this.router.navigate(['']);
+      })
+      .catch(reason => {
+        alert('login failed');
+        console.log(reason);
+      });
   }
 
 }
