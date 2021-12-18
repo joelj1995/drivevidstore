9ca6a224167c5e839827bfc9be235ec398bc2c25
Joel Johnston <colter13@gmail.com>
2021-12-17 20:51:38 -0700

    Basic tracer for worker code

---
+++ worker/Program.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+
+namespace DriveVidStore_Worker
+{
+    class Program
+    {
+        private const string UPLOAD_JOBS_QUEUE_NAME = "uploadjobs";
+        private static string StorageAccountConnectionString => Environment.GetEnvironmentVariable("StorageAccountConnectionString");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                QueueClient queueClient = new QueueClient(StorageAccountConnectionString, UPLOAD_JOBS_QUEUE_NAME);
+
+                if (queueClient.Exists())
+                {
+                    QueueMessage[] retrievedMessage = queueClient.ReceiveMessages();
+
+                    // Process (i.e. print) the message in less than 30 seconds
+                    if (retrievedMessage.Length > 0)
+                    {
+                        Console.WriteLine($"Dequeued message: '{retrievedMessage[0].Body}'");
+
+                        // Delete the message
+                        queueClient.DeleteMessage(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+                    }
+                }
+                System.Threading.Thread.Sleep(50);
+            }
+            
+        }
+    }
+}
