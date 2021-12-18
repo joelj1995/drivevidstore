26e83237ce05945c8faed7b03a71189b22bccb76
Joel Johnston <colter13@gmail.com>
2021-12-18 11:45:33 -0700

    Refactor job polling code

--- worker/Program.cs
+++ worker/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Azure.Storage.Queues;
 using Azure.Storage.Queues.Models;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 
 namespace DriveVidStore_Worker
 {
@@ -11,27 +13,52 @@ namespace DriveVidStore_Worker
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            while (true)
+            Console.WriteLine("Worker started!");
+            while (true) // TODO: Add some type of kill switch to gracefully shut down
             {
-                QueueClient queueClient = new QueueClient(StorageAccountConnectionString, UPLOAD_JOBS_QUEUE_NAME);
-
-                if (queueClient.Exists())
-                {
-                    QueueMessage[] retrievedMessage = queueClient.ReceiveMessages();
-
-                    // Process (i.e. print) the message in less than 30 seconds
-                    if (retrievedMessage.Length > 0)
-                    {
-                        Console.WriteLine($"Dequeued message: '{retrievedMessage[0].Body}'");
-
-                        // Delete the message
-                        queueClient.DeleteMessage(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
-                    }
-                }
+                PollAndProcessJob();
                 System.Threading.Thread.Sleep(50);
             }
             
+        }
+
+        public static void PollAndProcessJob()
+        {
+            QueueClient queueClient = new QueueClient(StorageAccountConnectionString, UPLOAD_JOBS_QUEUE_NAME);
+
+            if (queueClient.Exists())
+            {
+                QueueMessage[] retrievedMessage = queueClient.ReceiveMessages();
+
+                // Process (i.e. print) the message in less than 30 seconds
+                if (retrievedMessage.Length > 0)
+                {
+                    // TODO: Make sure timeout is aligned with the max time to process job
+                    Console.WriteLine($"Dequeued message: '{retrievedMessage[0].Body}'");
+                    var messageBodyText = retrievedMessage[0].Body.ToString();
+                    var messageBody = JsonConvert.DeserializeObject<Dictionary<string, string>>(messageBodyText);
+
+                    var jobUserId = messageBody["User"];
+                    var jobId = messageBody["Identifier"];
+                    var jobApiKey = messageBody["ApiKey"];
+
+                    var jobDataPath = DownloadJobAndReturnPath(jobUserId, jobId);
+                    UploadJobDataToDrive(jobDataPath, jobApiKey);
+
+                    // Delete the message
+                    queueClient.DeleteMessage(retrievedMessage[0].MessageId, retrievedMessage[0].PopReceipt);
+                }
+            }
+        }
+
+        public static string DownloadJobAndReturnPath(string userId, string identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void UploadJobDataToDrive(string jobDataPath, string jobApiKey)
+        {
+            throw new NotImplementedException();
         }
     }
 }
