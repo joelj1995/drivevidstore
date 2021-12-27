79af2f5b8fc2bb698d46d7b468e75c33355159a4
Joel Johnston <colter13@gmail.com>
2021-12-27 14:52:43 -0700

    Implement stylized upload button

--- app/src/app/main-content/main-content.component.ts
+++ app/src/app/main-content/main-content.component.ts
@@ -30,6 +30,13 @@ export class MainContentComponent implements OnInit {
     this.apiKey = null;
   }
 
+  uploadFileChanged(files: FileList) {
+    const fileToUpload = files.item(0);
+    if (fileToUpload) { // didn't cancel the operation
+
+    }
+  }
+
   getAuthToken() {
     // TODO: inject the client id and redirect
     window.location.href = "https://accounts.google.com/o/oauth2/v2/auth?client_id=968753614509-8pnb23be87158145scn9q1e7aa97pcbu.apps.googleusercontent.com&redirect_uri=http://localhost:4200&response_type=token&scope=https://www.googleapis.com/auth/drive.file";
